b1da1ed2db0d5743f2dd002d0e7286c110cd131d
OlesiaKubska <kublesia0908@gmail.com>
2023-09-09 00:57:11 +0200

    add pages and update App.jsx and index.js

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,24 @@
-export const App = () => {
+import React, { lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+const Home = lazy(() => import('../pages/Home'));
+const Movies = lazy(() => import('../pages/Movies'));
+const MovieDetails = lazy(() => import('../pages/MovieDetails'));
+const Cast = lazy(() => import('../pages/Cast'));
+const Reviews = lazy(() => import('../pages/Reviews'));
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={Home} />
+        <Route path="/movies" element={Movies} />
+        <Route path="/movies/:movieId" element={MovieDetails} />
+          <Route path="/movies/:movieId/cast" element={Cast} />
+          <Route path="/movies/:movieId/reviews" element={Reviews} />
+      </Routes>
+    </>
   );
-};
+}
+
+export default App;
