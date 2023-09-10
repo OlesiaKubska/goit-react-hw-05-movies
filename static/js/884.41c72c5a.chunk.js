0f0c5cd894ab81cdf083633dfaf1db05ba335f01
OlesiaKubska <120682505+OlesiaKubska@users.noreply.github.com>
2023-09-10 15:42:37 +0000

    Deploying to gh-pages from @ OlesiaKubska/goit-react-hw-05-movies@ca8761b71926dbde8b8da776e4474a29dc7e4bd7 🚀

---
+++ static/js/884.41c72c5a.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[884],{884:function(e,i,t){t.r(i);t(791);var s=t(184);i.default=function(){return(0,s.jsx)("div",{children:"MovieDetails"})}}}]);
+//# sourceMappingURL=884.41c72c5a.chunk.js.map
